98f9e690ff0a890270a7093b1cbd7d364ce24393
Michael Fenimore <mfenimore70@gmail.com>
2017-06-06 16:37:38 -0400

    Update to variable naming prefix
    Fixed usta_ to usat_ (USA Today)

--- usa-today/usat-class.php
+++ usa-today/usat-class.php
@@ -26,24 +26,24 @@ if ( !class_exists( 'Usa_Today' ) ) {
 				'post_status' => 'publish',
 			);
 
-			$usta_recent_article = wp_get_recent_posts( $args );
+			$usat_recent_article = wp_get_recent_posts( $args );
 
 			//Make sure no empty values are returned. If some are empty they can be given default values. Default values will need business approval.
-			if ( !empty( $usta_recent_article ) && is_array( $usta_recent_article ) ) {
-				$usta_latest_post['title'] = !empty( $usta_recent_article[0]['post_title'] ) ? $usta_recent_article[0]['post_title'] : 'Most Recent Article' ;
-				$usta_latest_post['author'] = !empty( $usta_recent_article[0]['post_author'] ) ? get_the_author_meta( 'display_name', $usta_recent_article[0]['post_author'] ) : 'USA Today' ;
-				$usta_latest_post['thumbnail'] = get_the_post_thumbnail( $usta_recent_article[0][ID]) ;
+			if ( !empty( $usat_recent_article ) && is_array( $usat_recent_article ) ) {
+				$usat_latest_post['title'] = !empty( $usat_recent_article[0]['post_title'] ) ? $usat_recent_article[0]['post_title'] : 'Most Recent Article' ;
+				$usat_latest_post['author'] = !empty( $usat_recent_article[0]['post_author'] ) ? get_the_author_meta( 'display_name', $usat_recent_article[0]['post_author'] ) : 'USA Today' ;
+				$usat_latest_post['thumbnail'] = get_the_post_thumbnail( $usat_recent_article[0][ID]) ;
 
 				//Multiple categories can be returned for a post. This grabs the first one. If necessary can check to make sure its a parent category.
-				$categories = wp_get_post_categories( $usta_recent_article[0][ID] );
+				$categories = wp_get_post_categories( $usat_recent_article[0][ID] );
 				if ( !empty( $categories ) ) {
 					$first_category_array = get_category( $categories[0] );
-					$usta_latest_post['category_name'] = $first_category_array->name;
+					$usat_latest_post['category_name'] = $first_category_array->name;
 				}
 
 				//Convert the posts timestamp into unix time to get time difference
-				$unix_timestamp = strtotime( $usta_recent_article[0]['post_modified_gmt'] );
-				$usta_latest_post['time'] = human_time_diff( $unix_timestamp, current_time( 'timestamp' ) ) . ' ago';
+				$unix_timestamp = strtotime( $usat_recent_article[0]['post_modified_gmt'] );
+				$usat_latest_post['time'] = human_time_diff( $unix_timestamp, current_time( 'timestamp' ) ) . ' ago';
 			}
 
 			//Define what tags are valid when outputting image tag (wp_kses). VIP requires escaping.
@@ -56,14 +56,14 @@ if ( !class_exists( 'Usa_Today' ) ) {
 			);
 
 			$html = '<div id=usat_main_container>
-				<div class="usat-thumbnail">' . wp_kses( $usta_latest_post['thumbnail'], $valid_image_tags ) . '</div>
-				<div class="usat-category">' . esc_html( $usta_latest_post['category_name'] ) . '
-					<span class="usat-time mini"> | ' . esc_html( $usta_latest_post['time'] ) . '</span>
+				<div class="usat-thumbnail">' . wp_kses( $usat_latest_post['thumbnail'], $valid_image_tags ) . '</div>
+				<div class="usat-category">' . esc_html( $usat_latest_post['category_name'] ) . '
+					<span class="usat-time mini"> | ' . esc_html( $usat_latest_post['time'] ) . '</span>
 				</div>
-				<h2 class="usat-title">' . esc_html( $usta_latest_post['title'] ) . '</h2>
+				<h2 class="usat-title">' . esc_html( $usat_latest_post['title'] ) . '</h2>
 				<div class="usat-metadata">
-					<div class="usat-byline">By: <span class="usat-name">' . esc_html( $usta_latest_post['author'] ) . '</span>
-						<span class="usat-time">' . esc_html( $usta_latest_post['time'] ) . '</span>
+					<div class="usat-byline">By: <span class="usat-name">' . esc_html( $usat_latest_post['author'] ) . '</span>
+						<span class="usat-time">' . esc_html( $usat_latest_post['time'] ) . '</span>
 					</div>
 				</div>
 			</div>';
